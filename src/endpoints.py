e3f213ae50fda09188615a18803272276f85b1c0
alessiogiovagnini <alessio.giovagnini@usi.ch>
2023-11-16 14:58:36 +0100

    added some queries, TODO not finished

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,21 +1,16 @@
 from flask import Flask
 from flask import request, jsonify
-from pymongo import MongoClient
-import datetime
-from pathlib import Path
+from mongoQuery import add_picture
 
 app = Flask(__name__)
-client = MongoClient(host="localhost", port=27017)
-db = client.locumVisiumDB
 
 
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files.get("picture")
-    path: Path = Path(f"data/{file.name}-{datetime.datetime.now()}.png")
-    file.save(dst=path.absolute())
-    collection = db.pictures
-    collection.insert_one({"path": path.as_posix()})
+    # TODO also pass coordinates and text
+    res = add_picture(file=file)
+    # TODO check the res and return appropriate code
     return jsonify({"code": 201})
 
 
---
+++ src/mongoQuery.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from pymongo import MongoClient
+import datetime
+
+client = MongoClient(host="localhost", port=27017)
+db = client.locumVisiumDB
+collection = db.pictures
+
+
+# TODO save picture with text
+def add_picture(file, longitude: float, latitude: float, text: str):
+    path: Path = Path(f"data/{file.name}-{datetime.datetime.now()}.png")
+    file.save(dst=path.absolute())
+
+    mongo_object = {
+        "path": path.as_posix(),
+        "text": text,
+        "location": {
+            "type": "Point",
+            "coordinates": [longitude, latitude]
+        }
+    }
+    res = collection.insert_one(mongo_object)
+    return res
+
+
+# TODO return pictures based on coordinates
+def get_pictures(longitude: float, latitude: float, max_distance: float):
+
+    res = collection.find({
+        "location": {
+            "$near": {
+                "$geometry": {
+                    "type": "Point",
+                    "coordinates": [longitude, latitude]
+                },
+                "$minDistance": 0,
+                "$maxDistance": max_distance
+            }
+        }
+    })
+    return res
+
+
